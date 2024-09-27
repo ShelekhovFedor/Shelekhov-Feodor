c05a808e1816f018d4c96cec451322b0e2cede67
ShelekhovFedor <f.shelekhov@gmail.com>
2024-09-27 17:09:10 +0300

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,11 @@
+
+r=None
+s=1
+while True:
+    r=input()
+    if r=='стоп':
+        break
+    s=s*r**2
+    print(s)
+    s=1
+
---
+++ 13.py
@@ -0,0 +1,9 @@
+n=int(input())
+s=1
+for i in range(n+1):
+    a=int(input())
+    b=int(input())
+    s=s*a*b
+    print(s)
+    s=1
+
