339df0a1a3873aa841e9ad8a51232bda67d33b75
ShelekhovFedor <f.shelekhov@gmail.com>
2024-11-08 20:56:36 +0300

    Update 12.py

--- lab2/12.py
+++ lab2/12.py
@@ -1,12 +1,28 @@
 
-r=None
+import math as m
+print("1-площадь квадрата"
+      "2-площадь круга"
+      "3-площадь треугольника")
+x=int(input())
+print("Введите количество значений")
+n=int(input())
 s=1
-while True:
-    r=input()
-    if str(r)=='стоп':
-        break
-    s=s*int(r)**2
-
-    print(s)
-    s=1
+if x==1:
+    for i in range(n):
+        r=int(input())
+        s=r**2
+        print(s)
+        s=1
+elif x==2:
+    for i in range(n):
+        r = int(input())
+        s=m.pi*r**2
+        print(s)
+        s=1
+elif x==3:
+    for i in range(n):
+        r = int(input())
+        s=(r**2*m.sqrt(3))/4
+        print(s)
+        s=1
 
