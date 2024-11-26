80dd25e9f531dc409ac4e9e0c5fd63675bb3c87a
ShelekhovFedor <f.shelekhov@gmail.com>
2024-11-26 20:14:02 +0300

    Update лаб4ур1номер21.py

--- lab4/лаб4ур1номер21.py
+++ lab4/лаб4ур1номер21.py
@@ -1,10 +1,12 @@
 
 x=0
-w=[[9, 6, 9, 2, 9], [3, 4, 5, 5, 9], [10, 5, 2, 9, 4], [8, 6, 5, 3, 9], [8, 1, 3, 3, 10], [3, 1, 7, 2, 7]]
-print(w)
-for i in range(6):
+w=[[9, 6, 9, 2, 9, 1], [3, 4, 5, 5, 9,5], [10, 5, 2, 9, 4, 2], [8, 6, 5, 3, 9,2], [8, 1, 3, 3, 10,9]]
+for i in range(len(w)):
+    print(w[i])
+for i in range(5):
     x = max(w[i])
     w[i].insert(5,x)
     x=None
-print(w)
-
+print()
+for i in range(len(w)):
+    print(w[i])
