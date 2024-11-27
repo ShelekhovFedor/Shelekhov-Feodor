e7819e1de069f99c73f7fcc8bc491572d29355f6
ShelekhovFedor <f.shelekhov@gmail.com>
2024-11-27 18:34:33 +0300

    Update лаб4ур2номер3.py

--- lab4/лаб4ур2номер3.py
+++ lab4/лаб4ур2номер3.py
@@ -1,19 +1,37 @@
-import random as r
-q=[[ r.randint(0,10) for i in range(10)] for j in range(5)]
+q=[[5, 0, 8, 5, 8],
+[2, 6, 4, 1, 10],
+[3, 10, 9, 8, 3],
+[7, 2, 5, 5, 8],
+[6, 6, 3, 5, 9],
+[8, 7, 8, 5, 2],
+[1, 9, 1, 9, 8],
+[0, 1, 1, 3, 5],
+[9, 4, 1, 8, 4],
+[10, 6, 5, 8, 1]]
 for i in q:
     for j in i:
         print(j, end = ' ')
     print()
+r=[]
+e=None
+o=0
+u=None
 for i in range(5):
-    x=max(q[i])
-    w=q[i].index(x)
-    if w<=4:
-        e=sum(q[i][w+1:])
-        print(e)
-        q[i].pop(0)
-        q[i].insert(0,e)
-for i in q:
-    for j in i:
-        print(j, end = ' ')
-    print()
+    for j in range(10):
+        r.append(q[j][i])
+    e=max(r)
+    u = r.index(e)
+
+    print(u)
+
+    if u>=0 and u<5:
+        o=sum(r[(u+1):])
+        q[0].pop(i)
+        q[0].insert(i,o)
+        o=0
+        u=0
+    for s in range(len(r)):
+        r.pop()
+for i in range(len(q)):
+    print(q[i])
 
