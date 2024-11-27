3a2037bb02c865572e712bfe27dcf5b14abd8861
ShelekhovFedor <f.shelekhov@gmail.com>
2024-11-27 18:52:47 +0300

    Update лаба4ур3номер9......py

--- lab4/лаба4ур3номер9......py
+++ lab4/лаба4ур3номер9......py
@@ -1,11 +1,10 @@
-import random as rd
-q=[[-3, -2, 5, -1, 10], [10, -7, -2, -3, 7], [-4, 10, -4, 1, 5], [-4, -3, 9, -8, -10], [3, 5, 2, 5, 7], [5, 8, 9, -9, 4], [5, -9, 0, -4, 2]]
+q=[[-3, -2, 5, -1, 8,-1,9], [-7, -7, 2, -3, 7,-9,4], [-4, -5, -4, 1, 5,-7,-2,], [-4, -3, -9, -8, 1,3,7], [3, -5, -2, -5, -7,-8,-9]]
 for i in q:
     print(i,sep="")
 e=0
 r=[]
-for i in range(5):
-    for j in range(7):
+for i in range(7):
+    for j in range(5):
         if q[j][i]<0:
             e+=1
     r.append(e)
@@ -13,12 +12,17 @@ for i in range(5):
 print()
 print(r)
 print()
-for i in range(5):
+for i in range(7):
     x=min(r[i:])
     t=r.index(x)
     r[i], r[t] = r[t], r[i]
-    for j in range(7):
+    r[i]=99
+    for j in range(5):
         q[j][i] , q[j][t] = q[j][t] , q[j][i]
+    print(r)
+print(r)
+print()
 for i in q:
     print(i,sep="")
 print(r)
+
