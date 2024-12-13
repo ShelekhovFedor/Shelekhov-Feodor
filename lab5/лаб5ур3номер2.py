62707b69355e3223b817e0604d3c4e2646a799e2
ShelekhovFedor <f.shelekhov@gmail.com>
2024-12-13 13:45:46 +0300

    Update лаб5ур3номер2.py

--- lab5/лаб5ур3номер2.py
+++ lab5/лаб5ур3номер2.py
@@ -1,13 +1,21 @@
 q=[[0,1,2],[8,4,5],[2,0,3]]
 w=1
+for i in range(len(q)):
+    print(q[i])
+print()
 def task(q,w):
     for i in range(len(q)):
         if w%2!=0:
-            q[i].sort()
-            q[i].reverse()
+            for j in range(len(q[0])):
+                for t in range(len(q[0])):
+                    if q[i][j]>q[i][t]:
+                        q[i][j],q[i][t]=q[i][t],q[i][j]
             w+=1
         else:
-            q[i].sort()
+            for o in range(len(q[0])):
+                for u in range(len(q[0])):
+                    if q[i][o]<q[i][u]:
+                        q[i][o],q[i][u] = q[i][u] ,q[i][o]
             w+=1
     return q
 for i in range(len(task(q,w))):
