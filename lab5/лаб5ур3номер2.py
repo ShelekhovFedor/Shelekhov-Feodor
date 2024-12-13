4cb391f6bfb1e64e1119b1c710c3449d010ba766
ShelekhovFedor <f.shelekhov@gmail.com>
2024-12-13 12:27:57 +0300

    Update лаб5ур3номер2.py

--- lab5/лаб5ур3номер2.py
+++ lab5/лаб5ур3номер2.py
@@ -1,11 +1,14 @@
 q=[[0,1,2],[8,4,5],[2,0,3]]
 w=1
-for i in range(len(q)):
-    if w%2!=0:
-        q[i].sort()
-        q[i].reverse()
-        w+=1
-    else:
-        q[i].sort()
-        w+=1
-print(q)
+def task(q,w):
+    for i in range(len(q)):
+        if w%2!=0:
+            q[i].sort()
+            q[i].reverse()
+            w+=1
+        else:
+            q[i].sort()
+            w+=1
+    return q
+for i in range(len(task(q,w))):
+    print(task(q,w)[i])
