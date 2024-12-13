7a3e65414e572507a2f5375594a016a2cf886831
ShelekhovFedor <f.shelekhov@gmail.com>
2024-12-13 12:28:39 +0300

    Update лаб5ур1номер3.py

--- lab5/лаб5ур1номер3.py
+++ lab5/лаб5ур1номер3.py
@@ -5,13 +5,22 @@ a=1
 A=1.6
 t=1
 T=4
-if v*t+(a*t**2)/2>V*t+(A*t**2)/2:
-    print("За 1 час 1 проедет больше")
-else:
-    print("За 1 час 2 проедет больше")
-if  v*T+(a*T**2)/2>V*T+(A*T**2)/2:
-    print("За 4 час 1 проедет больше")
-else:
-    print("За 4 час 2 проедет больше")
-o=(2*(v-V))/(a-A)
-print(math.fabs(o))
+def result(v,V,a,A,t,T):
+    x1=v*t+(a*t**2)/2>V*t+(A*t**2)/2
+    x2 = v * T + (a * T ** 2) / 2 > V * T + (A * T ** 2) / 2
+    if x1:
+        return "За 1 час 1 проедет больше"
+    else:
+        return "За 1 час 2 проедет больше"
+def resul(v, V, a, A, t, T):
+    x2 = v * T + (a * T ** 2) / 2 > V * T + (A * T ** 2) / 2
+    if  x2:
+        return "За 4 час 1 проедет больше"
+    else:
+        return "За 4 час 2 проедет больше"
+def resu(v, V, a, A):
+    o=math.fabs((2*(v-V))/(a-A))
+    return o
+print(result(v, V, a, A, t, T))
+print(resul(v, V, a, A, t, T))
+print(resu(v, V, a, A))
