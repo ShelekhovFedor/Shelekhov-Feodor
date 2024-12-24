cdc955948b5f4f74711302b5c5c58fdc5c2f106a
ShelekhovFedor <f.shelekhov@gmail.com>
2024-12-24 18:25:58 +0300

    Update КР_лаб4ур1номер29.py

--- Контрольная 2/КР_лаб4ур1номер29.py
+++ Контрольная 2/КР_лаб4ур1номер29.py
@@ -1,7 +1,6 @@
-import random as rd
 import math as mh
 q=[[-2, 10, -2, -2, 5],
-[6, 1, -5, 3, -1],
+[6, -1, -5, 3, 1],
 [5, -2, -2, -5, 2],
 [1, -7, 0, 5, 7],
 [7, -1, 3, 5, 0]]
@@ -12,9 +11,13 @@ for i in range(len(q[1])):
     if abs(q[1][i])<x:
         x=abs(q[1][i])
 print(x)
-z=q[1].index(x)
-c=z+1
-print(z)
+for i in range(len(q[0])):
+    if abs(q[1][i])==x:
+        c=i+1
+        break
+if c>4:
+    c=0
+print(c)
 for i in range(len(q)):
     q[i].pop(c)
     i+=1
