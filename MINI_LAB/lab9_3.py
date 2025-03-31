0ed7b759c44ff8f16f9dbf74def732682607cdd3
ShelekhovFedor <f.shelekhov@gmail.com>
2025-03-31 13:11:59 +0300

    Update lab9_3.py

--- MINI_LAB/lab9_3.py
+++ MINI_LAB/lab9_3.py
@@ -1,33 +1,31 @@
 #3. На основании результатов соревнований по прыжкам в длину
 # (фамилии и результаты трех попыток) составить итоговый протокол
 # соревнований, считая, что в зачет идет лучший результат.
-first=open('first_sportsmen.txt','r')
-second=open('second_sportsmen.txt','r')
-third=open('third_sportsmen.txt','r')
-q=first.read()
-w=second.read()
-e=third.read()
-q=q.split(',')
-w=w.split(',')
-e=e.split(',')
-for i in range(len(q)):
+e=open('sportsmens.txt','r')
+
+w=e.read()
+
+q=w.split(',')
+
+
+for i in range(0,3):
     q[i]=int(q[i])
-for i in range(len(w)):
-    w[i]=int(w[i])
-for i in range(len(e)):
-    e[i]=int(e[i])
+for i in range(3,6):
+    q[i]=int(q[i])
+for i in range(6,9):
+    q[i]=int(q[i])
 r=q[0]
-for i in range(len(q)):
+for i in range(0,3):
     if q[i]>r:
         r=q[i]
-t=w[0]
-for i in range(len(w)):
-    if w[i]>t:
-        t=w[i]
-y=e[0]
-for i in range(len(e)):
-    if e[i]>y:
-        y=e[i]
+t=q[3]
+for i in range(3,6):
+    if q[i]>t:
+        t=q[i]
+y=q[6]
+for i in range(6,9):
+    if q[i]>y:
+        y=q[i]
 j=[]
 j.append('first_sportsmen:')
 j.append(str(r))
@@ -39,10 +37,8 @@ Results=open('Results.txt','w')
 print(j)
 for i in range(len(j)):
     Results.write(j[i])
-first.close()
+e.close()
 Results=open('Results.txt','r')
-second.close()
-third.close()
 k=Results.read()
 print(k)
 Results.close()
