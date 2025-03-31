ff40833213137bbf664c44d7f09e188e61589a6e
ShelekhovFedor <f.shelekhov@gmail.com>
2025-03-31 13:40:51 +0300

    Update minilab2_ex2_nb27.py

--- Mini lab 2/minilab2_ex2_nb27.py
+++ Mini lab 2/minilab2_ex2_nb27.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x=np.array([70,83,87,90,94,98,102,105,110,114,119])
-y=np.array([21,19,17,16,15,14,13,12,11,9,7])
+e=open('точки.txt','r')
+
+w=e.read()
+
+q=w.split(',')
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 fig, ax = plt.subplots()
-ax.scatter(x, y)
+ax.scatter(q[:11], q[11:])
 plt.show()
